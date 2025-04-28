2d2347a064c60c3a1469365846e9049c4ce39ce4
a <a>
2025-04-28 11:51:06 -0300

    tentativa 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,67 @@
 "use client";
 
-const Home = () => {
-  return (
-    <div>
-      <main>
-        <h1>React - conceitos básicos</h1>
-      </main>
-    </div>
+import React from "react";
+import { console } from "inspector";
+
+const Titulo = () => (
+          <h1 className="text-2xl font-bold mb-1">React - conceitos básicos</h1>
+);
+
+const SubTitulo = () => (
+          <h2 className="text-4xl font-bold mb-6">Lista de tarefas</h2>
+);
+
+function Cabecalho() {
+  return(
+          <div className="text-center">
+                  <Titulo />
+                  <SubTitulo />
+          </div>
   );
 }
+
+interface TarefaProps {
+  titulo: string;
+          concluido?:boolean;
+
+}
+
+
+const Tarefa: React.FC<TarefaProps> = ({titulo, concluido}) => {
+    
+                const classe = 'p-3 mb-3 rounded-lg shadow-md hover:cursor-pointer hover:border ${this.props.concluido ? "bg-gray-800 hover:bg-gray-800" : "bg-gray-400 hover:bg-gray-400"}';
+
+                return (
+                      
+
+
+                
+                        <div 
+                          className={classe}
+                          OnClick={() => console.log('A tarefa '${titulo}' foi clicada')}
+                        >
+                            <h3 className="text-xl font-bold">{titulo}</h3>
+                            <p className="text-sm">{concluido ? "Concluida" : "Pendente"}</p>
+                        </div>
+                );
+        
+}
+
+const Home = () => {
+  const tarefas = [
+      {id: 1, title: "delectus aut autem", completed: false},
+      {id: 2, title: "quis ut nam facilis et officia qui", completed: true},
+      {id: 3, title: "fugiat veniam minus", completed: false},
+  ];
+
+
+  return(
+    <div className="container mx-auto p-4">
+     <Cabecalho />
+     <Tarefa titulo={tarefas[0].title} concluido={tarefas[0].completed}/>
+     <Tarefa titulo={tarefas[1].title} concluido={tarefas[1].completed}/>
+    </div>
+  );
+};
 
 export default Home;
