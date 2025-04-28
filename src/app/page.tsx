baf7514dcaf273ac0c89fcb027aa4c1be48b8ab2
a <a>
2025-04-28 11:55:19 -0300

    oi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,7 +50,7 @@ const Tarefa: React.FC<TarefaProps> = ({titulo, concluido}) => {
 const Home = () => {
   const tarefas = [
       {id: 1, title: "delectus aut autem", completed: false},
-      {id: 2, title: "quis ut nam facilis et officia qui", completed: true},
+      {id: 2, title: "quis ut nam facilis et officia qui OIII", completed: true},
       {id: 3, title: "fugiat veniam minus", completed: false},
   ];
 
